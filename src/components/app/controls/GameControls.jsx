0b054ecf5d353775c4eaaed6154933006d4e240c
zhaned <zhanedmond@gmail.com>
2021-06-15 11:49:10 -0700

    implemented hover feature for all buildings

--- src/components/app/controls/GameControls.jsx
+++ src/components/app/controls/GameControls.jsx
@@ -30,20 +30,23 @@ const GameControls = ({
               onClick={unlockBuilding}
               disabled={((gold < goldRequired.lumberyard || user.lumberyard) ? true : false)}>Purchase Lumberyard ({`${goldRequired.lumberyard}`}g)
             </button>
-            <img className={style.hovertest} src="../../../../assets/lumberyard-info.png" alt="Lumberyard" />
+            <img className={style.hovertest} src="../../../../assets/info-cards/lumberyard-info.png" alt="Lumberyard" />
           </div> : ''
       }
     
       {
         (gold > (goldRequired.windmill * revealPercent)) ?
-          <button 
-            className={
-              ((gold < goldRequired.windmill) ? style.almost : ''), 
-              style.bigButton} 
-            value="windmill" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.windmill || user.windmill) ? true : false)}>Purchase Windmill ({`${goldRequired.windmill}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={
+                ((gold < goldRequired.windmill) ? style.almost : ''),
+                style.bigButton}
+              value="windmill"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.windmill || user.windmill) ? true : false)}>Purchase Windmill ({`${goldRequired.windmill}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/windmill-info.png" alt="windmill" />
+          </div> : ''
       }
       {
         (gold > (goldRequired.mine * revealPercent)) ?
@@ -55,68 +58,86 @@ const GameControls = ({
               onClick={unlockBuilding}
               disabled={((gold < goldRequired.mine || user.mine) ? true : false)}>Purchase Mine ({`${goldRequired.mine}`}g)
             </button>
-            <span className={style.hovertest1}>+5 gold/sec</span>
+            <img className={style.hovertest} src="../../../../assets/info-cards/mine-info.png" alt="mine" />
           </div> : ''
       }
       {
         (gold > (goldRequired.watermill * revealPercent)) ?
-          <button 
-            className={((gold < goldRequired.watermill) ? style.almost : ''), 
-            style.bigButton} 
-            value="watermill" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.watermill || user.watermill) ? true : false)}>Purchase Watermill ({`${goldRequired.watermill}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={((gold < goldRequired.watermill) ? style.almost : ''),
+              style.bigButton}
+              value="watermill"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.watermill || user.watermill) ? true : false)}>Purchase Watermill ({`${goldRequired.watermill}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/watermill-info.png" alt="watermill" />
+          </div> : ''
       }
       {
         (gold > (goldRequired.sawmill * revealPercent)) ?
-          <button 
-            className={((gold < goldRequired.sawmill) ? style.almost : ''), 
-            style.bigButton} 
-            value="sawmill" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.sawmill || user.sawmill) ? true : false)}>Purchase Sawmill ({`${goldRequired.sawmill}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={((gold < goldRequired.sawmill) ? style.almost : ''),
+              style.bigButton}
+              value="sawmill"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.sawmill || user.sawmill) ? true : false)}>Purchase Sawmill ({`${goldRequired.sawmill}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/sawmill-info.png" alt="sawmill" />
+          </div> : ''
       }
       {
         (gold > (goldRequired.farm * revealPercent)) ?
-          <button 
-            className={((gold < goldRequired.farm) ? style.almost : ''), 
-            style.bigButton} 
-            value="farm" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.farm || user.farm) ? true : false)}>Purchase Farm ({`${goldRequired.farm}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={((gold < goldRequired.farm) ? style.almost : ''),
+              style.bigButton}
+              value="farm"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.farm || user.farm) ? true : false)}>Purchase Farm ({`${goldRequired.farm}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/farm-info.png" alt="farm" />
+          </div> : ''
       }
       {
         (gold > (goldRequired.blacksmith * revealPercent)) ?
-          <button 
-            className={((gold < goldRequired.blacksmith) ? style.almost : ''), 
-            style.bigButton} 
-            value="blacksmith" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.blacksmith || user.blacksmith) ? true : false)}>Purchase Blacksmith ({`${goldRequired.blacksmith}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={((gold < goldRequired.blacksmith) ? style.almost : ''),
+              style.bigButton}
+              value="blacksmith"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.blacksmith || user.blacksmith) ? true : false)}>Purchase Blacksmith ({`${goldRequired.blacksmith}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/blacksmith-info.png" alt="blacksmith" />
+          </div> : ''
       }
       {
         (gold > (goldRequired.tavern * revealPercent)) ?
-          <button 
-            className={((gold < goldRequired.tavern) ? style.almost : ''), 
-            style.bigButton} 
-            value="tavern" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.tavern || user.tavern) ? true : false)}>Purchase Tavern ({`${goldRequired.tavern}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={((gold < goldRequired.tavern) ? style.almost : ''),
+              style.bigButton}
+              value="tavern"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.tavern || user.tavern) ? true : false)}>Purchase Tavern ({`${goldRequired.tavern}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/tavern-info.png" alt="tavern" />
+          </div> : ''
       }
       {
         (gold > (goldRequired.castle * revealPercent)) ?
-          <button 
-            className={((gold < goldRequired.castle) ? style.almost : ''), 
-            style.bigButton} 
-            value="castle" 
-            onClick={unlockBuilding} 
-            disabled={((gold < goldRequired.castle || user.castle) ? true : false)}>Purchase Castle ({`${goldRequired.castle}`}g)
-          </button> : ''
+          <div className={style.something}>
+            <button
+              className={((gold < goldRequired.castle) ? style.almost : ''),
+              style.bigButton}
+              value="castle"
+              onClick={unlockBuilding}
+              disabled={((gold < goldRequired.castle || user.castle) ? true : false)}>Purchase Castle ({`${goldRequired.castle}`}g)
+            </button>
+            <img className={style.hovertest} src="../../../../assets/info-cards/castle-info.png" alt="castle" />
+          </div> : ''
       }
     </div>);
 
