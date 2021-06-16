9160586644cbad9e6623a3ce7f686a3b939fa154
Dominique Sakrisson <domsakrisson@gmailcom>
2021-06-16 10:58:18 -0700

    Animations added, canvas re-renders cut down

--- src/components/app/detriment/Detriment.jsx
+++ src/components/app/detriment/Detriment.jsx
@@ -1,51 +1,87 @@
 /* eslint-disable max-len */
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from '../style.css';
  
 const Detriment = ({ detriment }) => {
-  console.log(detriment);
+  // console.log(detriment);
   return(
-    <p>
-        Current detrimental effects are:
+    <div className={style.detDiv}>
+        <h2>Current detrimental effects are:</h2>
       {Object.keys(detriment).map(item => {
         if(detriment[item].active){
           switch(item) {
             case 'termites' : {
-              console.log(item);
-              return <p> {item} -1 gold/s </p>;          
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item} -1 gold/s </p>;          
             }
             case 'failedCrops' : {
-              console.log(item);
-              return <p> {item}: -10 gold/s </p>;           
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: -10 gold/s </p>;           
             }
             case 'caveIn' : {
-              console.log(item);
-              return <p> {item}: -20 gold/s </p>;
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: -20 gold/s </p>;
             }
             case 'flood' : {
-              console.log(item);
-              return <p> {item}: -10k gold </p>;
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: -10k gold </p>;
             }
             case 'osha' : {
-              console.log(item);
-              return <p> {item}: -50k gold -20% gold/s </p>;
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: -50k gold -20% gold/s </p>;
             }
             case 'peta' : {
-              console.log(item);
-              return <p> {item}: -10% gold/s </p>;
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: -10% gold/s </p>;
             }
             case 'bandits' : {
-              console.log(item);
-              return <p> {item}: -75% total gold </p>;
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: -75% total gold </p>;
             }
             case 'arson' : {
-              console.log(item);
-              return <p> {item}: Burnt down tavern </p>;
+              // console.log(item);
+              return <p className={style.activeDetriment}> {item}: Burnt down tavern </p>;
+            }
+          }
+        } else if(!detriment[item].active) {
+          switch(item) {
+            case 'termites' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item} -1 gold/s </p>;          
+            }
+            case 'failedCrops' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: -10 gold/s </p>;           
+            }
+            case 'caveIn' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: -20 gold/s </p>;
+            }
+            case 'flood' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: -10k gold </p>;
+            }
+            case 'osha' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: -50k gold -20% gold/s </p>;
+            }
+            case 'peta' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: -10% gold/s </p>;
+            }
+            case 'bandits' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: -75% total gold </p>;
+            }
+            case 'arson' : {
+              // console.log(item);
+              return <p className={style.inactiveDetriment}> {item}: Burnt down tavern </p>;
             }
           }
         }
       })}
-    </p>
+    </div>
   );
 };
 
--- src/components/app/hud/Clock.jsx
+++ src/components/app/hud/Clock.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react'
 import PropTypes from 'prop-types';
+import style from '../style.css'
 
 const Clock = ({ gameTime }) => {
   let hour, minute, second;
@@ -9,18 +10,18 @@ const Clock = ({ gameTime }) => {
   minute = Math.floor((gameTime - (hour * 3600)) / 60);
   second = gameTime - (hour * 3600) - (minute * 60);
   
-  if(String(hour).length < 2){
+  if (String(hour).length < 2){
     hour = `0${hour}`;
   }
-  if(String(minute).length < 2){
+  if (String(minute).length < 2){
     minute = `0${minute}`;
   }
-  if(String(second).length < 2){
+  if (String(second).length < 2){
     second = `0${second}`;
   }
 
   return (
-    <div style={{ backgroundColor: "grey", width: "150px", textAlign: "center" }}>
+    <div className={style.clock}>
       <img src="../../assets/square-clock.png" alt="Game Clock" width="30px"/>
        {hour }:{ minute}:{second}
     </div>
