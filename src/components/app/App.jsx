361d791dfeeba451a871e339dc01e6fee1702f8d
zhaned <zhanedmond@gmail.com>
2021-06-16 14:51:06 -0700

    handling auth stuff WIP

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Game from '../../containers/Game';
 import TitlePage from '../../containers/TitlePage';
 import Register from '../login/Register';
 import SignIn from '../login/SignIn';
 
+const AUTH = 'AUTH';
+
+const putAuthinLocal = (booger) => {
+  localStorage.setItem(AUTH, JSON.stringify(booger));
+};
+
+const getAuthFromLocal = () => {
+  const auth1 = localStorage.getItem(AUTH);
+  if (auth1) return JSON.parse(auth1);
+  return {};
+};
+
 export default function App() {
+  const [auth, setAuth] = useState({
+    auth: getAuthFromLocal()
+  });
+
+
+  const handleAuthChange = (authy) => {
+    putAuthinLocal(authy);
+    const auth1 = getAuthFromLocal();
+    console.log(auth1);
+    setAuth({
+      auth: auth1
+    });
+    console.log(auth, ' this is the current state');
+  };
+  
+
   return (
     <Switch>
       <Route
@@ -16,17 +44,17 @@ export default function App() {
       <Route
         path="/register"
         exact
-        render={(routerProps) => <Register {...routerProps} />}
+        render={(routerProps) => <Register {...routerProps} handleAuth={handleAuthChange} />}
       />
       <Route
         path="/login"
         exact
-        render={(routerProps) => <SignIn {...routerProps} />}
+        render={(routerProps) => <SignIn {...routerProps} handleAuth={handleAuthChange}/>}
       />
       <Route
         path="/game"
         exact
-        render={(routerProps) => <Game {...routerProps} />}
+        render={(routerProps) => <Game {...routerProps} auth={auth}/>}
       />
     </Switch>
   );
