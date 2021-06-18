641f24a04ca7351621418eb1f98f1ce40d08a5bc
Dominique Sakrisson <domsakrisson@gmailcom>
2021-06-18 15:30:10 -0700

    'update'

--- src/gameUtils/detrimentUtils.js
+++ src/gameUtils/detrimentUtils.js
@@ -72,6 +72,7 @@ export function detrimentRoll(
             });
             break;
           }
+          
           case 'osha': {
             setGold((prevGold) => {
               prevGold = prevGold - 12000;
